1b09a642a0f2437ad7a829eab35245735ab78243
Vinícius Faria <viniciusfdj@gmail.com>
2015-12-24 17:15:44 -0200

    Estruturas condicionais adicionadas

--- js/imc.js
+++ js/imc.js
@@ -9,4 +9,18 @@ function calcularIMC(){
 
     var imc = kilos / (altura * altura);
     formulario.imc.value = imc.toFixed(2);
+
+    if(imc <= 20){
+        alert('Abaixo do Peso!')
+    }else if(imc <= 25){
+        alert('Peso Ideal!')
+    }else if(imc <= 30){
+        alert ('Sobrepeso!')
+    }else if (imc <= 40) {
+        alert('Obesidade Severa!')
+    }else if (imc <= 50) {
+        alert('Obesidade Mórbida')
+    }else{
+        alert('Super Obesidade!')
+    }
 }
